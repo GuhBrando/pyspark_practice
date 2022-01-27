37f7aeed234193b5410b06334f848986353b1406
GuhBrando <gustavo@gf2.com.br>
2022-01-27 00:45:29 -0300

    Managing nulls
    Tratando nulos em dataframe.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import X
 import findspark
 from numpy import where
 import pyspark as spark
@@ -21,7 +22,7 @@ def openFile(fileName):
 def main():
     dfMovies = openFile("movies.json")
 
-    #Datas
+    # Datas
     moviesWithReleaseDates = dfMovies.select(col("Title"), to_date(col("Release_Date"), "dd-MMM-yy").alias("Actual_Release"))
     moviesWithReleaseDates = moviesWithReleaseDates\
         .withColumn("Today", current_date())\
@@ -33,8 +34,7 @@ def main():
     # Estruturas 
     # Operação com Coluna
     dfMovies.select(col("Title"), struct(col("US_Gross"), col("Worldwide_Gross")).alias("Profit"))\
-        .select(col("Title"), col("Profit").getField("US_Gross").alias("US_Profit"))\
-        .show()
+        .select(col("Title"), col("Profit").getField("US_Gross").alias("US_Profit"))
     
     # Com expressão de string
     dfMovies.selectExpr("Title", "(US_Gross, Worldwide_Gross) as Profit")\
@@ -47,8 +47,33 @@ def main():
         expr("Title_Words[0]"),
         size(col("Title_Words")),
         array_contains(col("Title_Words"), "Love")
-    ).show()
+    )
 
+
+    ## Lidando com Nulls
+    # Selecionar valor nao nulo entre 2 colunas
+    dfMovies.select(
+        col("Title"),
+        col("Rotten_Tomatoes_Rating"),
+        col("IMDB_Rating"),
+        coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating") * 10)
+    )
+
+    # Checar nulos
+    dfMovies.select("*").where(col("Rotten_Tomatoes_Rating").isNull())
+
+    # Ordernar baseado nos nulos
+    dfMovies.orderBy(col("IMDB_Rating").desc_nulls_last())
+
+    # Remover Nulos
+    dfMovies.select("Title", "IMDB_Rating").na.drop()
+
+    # Replace em nulos
+    dfMovies.na.fill(0, ["IMDB_Rating", "Rotten_Tomatoes_Rating"])
+    dfMovies.na.fill({"IMDB_Rating": 0,
+    "Rotten_Tomatoes_Rating": 10,
+    "Director": "Unkown"}
+    ).show()
 
 if __name__ == "__main__":
     main()
